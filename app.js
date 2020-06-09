6c8883ebb30ad7dd6cea992831c7e4c563c9146b
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-06-09 15:04:21 -0700

    receive user input and get response from external server using api

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const https = require("https");
-
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/", function(req, res){
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Benin City&appid=a2aff84845f89e61d6d247457132c6c2&units=metric";
+
+res.sendFile(__dirname + "/index.html");
+
+});
+
+
+app.post("/", function(req, res){
+
+const query= req.body.cityName;
+const apiKey="a2aff84845f89e61d6d247457132c6c2";
+const units = "metric";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query+"&appid="+apiKey+"&units="+units+"";
 
 //use native node http to make get request from the API
 
@@ -13,19 +26,24 @@ https.get(url,function(response){
 console.log(response.statusCode);
 
 response.on("data", function(data){
-  const weatherData = JSON.parse(data);
-    const weatherDescription = weatherData.weather[0].description;
-    const temp = weatherData.main.temp;
+const weatherData = JSON.parse(data);
+  const weatherDescription = weatherData.weather[0].description;
+  const temp = weatherData.main.temp;
+  const icon =weatherData.weather[0].icon;
+  const imageUrl = "http://openweathermap.org/img/wn/"+icon+ "@2x.png";
 
-
-      res.write("<p> The weather is currently " +weatherDescription+". </p>");
-    res.write("<h1>The current temperature in Benin City is " + temp + " degree Celcius.</h1>");
-      res.send();
+    res.write("<p> The weather is currently " +weatherDescription+". </p>");
+  res.write("<h1>The current temperature in "+query+" is " + temp + " degree Celcius.</h1>");
+  res.write("<img src="+imageUrl+">");
+    res.send();
 })
 
 })
 
-});
+
+console.log("Post request received!");
+})
+
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
